055e3bfb46ec5db75ec5eca628287ccc9e1140ec
VM <mogolivskiy@gmail.com>
2015-11-18 17:38:38 +0200

    Implement ATM and InnerCash con/de-structors

--- ATM.cpp
+++ ATM.cpp
@@ -1,1 +1,38 @@
 #include "ATM.h"
+
+#include <iostream>
+
+ATM::ATM():
+    _sesionKey(""),
+    _innerCash(new InnerCash())
+{
+#ifndef NDEBUG
+    cout << "ATM created." << endl;
+#endif
+}
+ATM::~ATM()
+{
+#ifndef NDEBUG
+    cout << "ATM deleated." << endl;
+#endif
+    delete _innerCash;
+}
+
+ATM& ATM::getInstance()
+{
+    static ATM instance;
+    return instance;
+}
+
+ATM::InnerCash::InnerCash():
+    _pockets(new Pocket[4])
+{
+#ifndef NDEBUG
+    cout << _pockets->_quantity << endl;
+#endif
+
+}
+ATM::InnerCash::~InnerCash()
+{
+    delete [] _pockets;
+}
--- ATM.h
+++ ATM.h
@@ -1,3 +1,8 @@
+/*
+ * ATM.h
+ * Author: Vitalii Moholivskyi
+ *
+ */
 #ifndef _ATM_H_
 #define _ATM_H_
 
@@ -8,26 +13,24 @@ class ATM
 {
 public:
     class InnerCash;
+    ~ATM();
     bool isBlocked();
     bool logIn(const string cardN, const size_t PIN);
     bool logOut();
+    bool changePIN(const size_t PIN);
     bool hasAdvert();
     string getAdvert();
     double getBalance();
     bool takeMoney(const double amount, bool useCreditMoney = false);
     bool getDataAbout(const string cardN);
     bool sendMoneyTo(const string cardN);
-    bool changePIN(const size_t PIN);
-    static ATM& getInstance()
-    {
-        static ATM instance;
-        return instance;
-    }
+
+    static ATM& getInstance();
 private:
-    string _cardN;
+    string _sesionKey;
     InnerCash * _innerCash;
 
-    ATM(){};
+    ATM();
     ATM(const ATM&);
     ATM& operator=(const ATM&);
 
@@ -35,8 +38,18 @@ private:
 
 class ATM::InnerCash
 {
-    InnerCash(){};
-    ~InnerCash(){};
+public:
+    struct Pocket;
+    InnerCash();
+    ~InnerCash();
+private:
+    Pocket* _pockets;
+};
+
+struct ATM::InnerCash::Pocket
+{
+    size_t _quantity;
+    size_t _value;
 };
 
 //#include "InnerCash.h"
